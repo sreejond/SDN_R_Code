816d9bca2921d0c4db92fa3aee5934e5fbc3a036
sreejond <sreejondy@gmail.com>
2018-11-19 19:37:00 -0700

    run random forest with 19 features selected by RFE

--- kdd_99.r
+++ kdd_99.r
@@ -208,12 +208,20 @@ library(randomForest)
 # Create final training data with the important features
 #sample_train=train_raw[sample(nrow(train_raw), replace=F, size=0.05*nrow(train_raw)), ]
 
-train_raw_imp_features <- train_raw[, c("srv_rerror_rate", "rerror_rate", "flag", "dst_host_rerror_rate" ,  
-               "logged_in", "dst_bytes", "src_bytes", "num_compromised",           
-               "dst_host_srv_count", "duration", "dst_host_same_src_port_rate", "dst_host_diff_srv_rate",   
-               "dst_host_count", "dst_host_srv_serror_rate", "count", "hot" ,                      
-               "dst_host_same_srv_rate", "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "serror_rate",                
-               "srv_serror_rate", "diff_srv_rate", "srv_count", "srv_diff_host_rate", "protocol_type", "label" )] 
+# selected 25 features
+# train_raw_imp_features <- train_raw[, c("srv_rerror_rate", "rerror_rate", "flag", "dst_host_rerror_rate" ,  
+#                "logged_in", "dst_bytes", "src_bytes", "num_compromised",           
+#                "dst_host_srv_count", "duration", "dst_host_same_src_port_rate", "dst_host_diff_srv_rate",   
+#                "dst_host_count", "dst_host_srv_serror_rate", "count", "hot" ,                      
+#                "dst_host_same_srv_rate", "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "serror_rate",                
+#                "srv_serror_rate", "diff_srv_rate", "srv_count", "srv_diff_host_rate", "protocol_type", "label" )] 
+
+# selected 19 features
+train_raw_imp_features <- train_raw[, c("flag", "dst_host_rerror_rate", "logged_in", "dst_bytes", 
+                                        "src_bytes", "num_compromised", "dst_host_srv_count", "duration", 
+                                        "dst_host_same_src_port_rate", "dst_host_diff_srv_rate", "dst_host_count", 
+                                        "dst_host_srv_serror_rate", "count", "hot", "dst_host_same_srv_rate", 
+                                        "dst_host_serror_rate", "protocol_type", "wrong_fragment", "srv_count" )] 
 
 inTrain <- createDataPartition(y = train_raw_imp_features$label, p = 0.5, list = FALSE)
 final_subset_train <- train_raw_imp_features[inTrain,]
